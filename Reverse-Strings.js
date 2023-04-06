5978bae4710920bcd16624efb8b9b53439965829
Byhanka <byhankacardoso@gmail.com>
2023-04-05 20:17:25 -0400

    daily code war

---
+++ Reverse-Strings.js
@@ -0,0 +1,14 @@
+//Complete the solution so that it reverses the string passed into it.
+
+/*
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+*/
+
+function solution(str){
+    let splitString = str.split('')
+    let reverseString = splitString.reverse()
+    let joinString = reverseString.join('')
+    
+    return joinString
+  }
